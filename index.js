6d2b838313330348f54c649f981766d605ecacfa
ni8walk3r <ni8walk3r@protonmail.com>
2019-05-30 14:15:26 +0530

    Demo

---
+++ index.js
@@ -0,0 +1,19 @@
+# "I'm a secret, please don't tell anyone about me"
+my_base64secret = 'SSdtIGEgc2VjcmV0LCBwbGVhc2UgZG9uJ3QgdGVsbCBhbnlvbmUgYWJvdXQgbWU='
+
+
+
+# "password1"
+my_hardcodedpassword = 'cGFzc3dvcmQx'
+
+
+# "password1" SHA256ed and expressed as a hex value
+my_sha256secret = '0b14d501a594442a01c6859541bcb3e8164d183d32937b851835442f69d5c94e'
+
+
+username = 'stephenfalken'
+password = 'joshua'
+
+
+my_json = 'ew0KICAgJ2hlbGxvJzogJ3dvcmxkJw0KfQ=='
+
