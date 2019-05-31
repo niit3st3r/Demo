85cd90fc41e46da0d3278b7fd170b77173c2ad5e
niit3st3r <51160962+niit3st3r@users.noreply.github.com>
2019-05-31 12:35:07 +0530

    Pull Request

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 # "I'm a secret, please don't tell anyone about me"
-my_base64secret = 'SSdtIGEgc2VjcmV0LCBwbGVhc2UgZG9uJ3QgdGVsbCBhbnlvbmUgYWJvdXQgbWU='
+my_base64secret = 'SSdtIGfEgc2VjcmV0LCBwbGVhc2UgZG9uJ3QgdGVsbCBhbnlvbmUgYWJvdXQgbWU='
 
 
 
 # "password1"
-my_hardcodedpassword = 'cGFzc3dvcmQx'
+my_hardcodedpassword = 'cGFzcf3dvcmQx'
 
 
 # "password1" SHA256ed and expressed as a hex value
-my_sha256secret = '0b14d501a594442a01c6859541bcb3e8164d183d32937b851835442f69d5c94e'
+my_sha256secret = '0b14d501a594f442a01c6859541bcb3e8164d183d32937b851835442f69d5c94e'
 
 
-username = 'stephenfalken'
-password = 'joshua'
+username = 'stephefnfalken'
+password = 'joshfua'
 
 
-my_json = 'ew0KICAgJ2hlbGxvJzogJ3dvcmxkJw0KfQ=='
+my_json = 'ew0KICAgJ2fhlbGxvJzogJ3dvcmxkJw0KfQ=='
 
