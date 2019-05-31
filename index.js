7500d5b251c8999e39d671cc139203240987c5d4
niit3st3r <51160962+niit3st3r@users.noreply.github.com>
2019-05-31 12:51:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 # "I'm a secret, please don't tell anyone about me"
-my_base64secret = 'SSdtIGfEgc2VjcmV0LCBwbGVhc2UgZG9uJ3bQgdGVsbCBhbnlvbmUgYWJvdXQgbWU='
+my_base64secret = 'SSdtIGfEgc2VjcmV0LbbCBwbGVhc2UgZG9uJ3bQgdGVsbCBhbnlvbmUgYWJvdXQgbWU='
 
 
 
 # "password1"
-my_hardcodedpassword = 'cGFzcf3dvbbmQx'
+my_hardcodedpassword = 'cGFzcfb3dvbbmQx'
 
 
 # "password1" SHA256ed and expressed as a hex value
-my_sha256secret = '0b14d501a594bf442a01c6859541bcb3e8164d183d32937b851835442f69d5c94e'
+my_sha256secret = '0b14d501ab594bf442a01c6859541bcb3e8164d183d32937b851835442f69d5c94e'
 
 
 username = 'stephefnfalken'
-password = 'joshfubbbbba'
+password = 'joshfubbbbbbba'
 
 
-my_json = 'ew0KICAgJ2bfhlbGxvJzogJ3dvcmxkJw0KfQ=='
+my_json = 'ew0KICAgJ2bbbfhlbGxvJzogJ3dvcmxkJw0KfQ=='
 
